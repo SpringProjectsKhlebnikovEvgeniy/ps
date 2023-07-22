6f8f22413878f42526932901dd23608157a064be
KhlebnikovEvgeniy <khebnikove@gmail.com>
2023-07-22 11:08:52 +0300

    Initial commit. Product Service.

---
+++ src/main/java/telran/java47/ProductServiceApplication.java
@@ -0,0 +1,13 @@
+package telran.java47;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+
+}
